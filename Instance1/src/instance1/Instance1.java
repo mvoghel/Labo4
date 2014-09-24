4e96ecfa1a5308d0b5d2de2eaa74dfecd7f9769e
gg291420 <gg291420@C39393.ens.uqam.ca>
2014-09-24 11:06:40 -0300

    Premier commit

---
+++ Instance1/src/instance1/Instance1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package instance1;
+
+/**
+ *
+ * @author gg291420
+ */
+public class Instance1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Salut Martin");
+    }
+    
+}
